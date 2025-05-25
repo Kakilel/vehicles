ba32144db6e905eaaede99a496955100ab7cd074
ckakilel@gmail.com <kakilelbarwett@gmail.com>
2025-05-26 00:46:20 +0300

    done with sign in page

---
+++ script.js
@@ -0,0 +1,89 @@
+const loginContainer = document.querySelector(".login-container");
+const registerContainer = document.querySelector(".register-container");
+const logEmail = document.querySelector(".log-mail")
+const regEmail = document.querySelector(".reg-mail")
+const logPassword = document.querySelector(".log-password")
+const regPassword = document.querySelector(".reg-password")
+const loginBtn = document.querySelector(".log-btn")
+const registerBtn = document.querySelector(".reg-btn")
+const userName = document.querySelector(".username");
+const links = document.querySelectorAll("#container a");
+const closeBtns = document.querySelectorAll(".close");
+
+
+
+
+// STYLING
+loginContainer.classList.add("active")
+
+links.forEach(link =>{
+    link.addEventListener("click", (e) =>{
+        e.preventDefault();
+
+        if(link.textContent.includes("Register")){
+            loginContainer.classList.remove("active");
+            registerContainer.classList.add("active");
+        }else if(link.textContent.includes("Sign In")){
+            loginContainer.classList.add("active")
+            registerContainer.classList.remove("active")
+        }
+    });
+});
+
+closeBtns.forEach(btn =>{
+    btn.addEventListener ("click", () =>{
+        loginContainer.classList.remove("active");
+        registerContainer.classList.remove("active");
+    });
+});
+
+// FILLING
+validateEmail = (email) => {
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    return pattern.test(email);
+}
+
+// login FORM
+loginContainer.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    const email = loginContainer.email.value;
+    const password = loginContainer.password.value;
+
+    if(!validateEmail(email)){
+        alert("Please enter a valid email.");
+        return;
+    }
+    if(password === ""){
+        alert("Isert a password!");
+        return;
+    }
+
+
+    console.log("Login Success:",{email,password});
+    alert("Login successful!");
+    loginContainer.reset();
+});
+
+registerContainer.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const email = registerContainer.regEmail.value;
+    const username = registerContainer.regUsername.value;
+    const password = registerContainer.regPassword.value;
+    if(!validateEmail(email)){
+        alert("Enter a valid Email")
+        return;
+    }
+    if (userName.length< 4){
+        alert("Username must be more than 4 characters ")
+        return;
+    }
+    if (password.length< 8){
+        alert("Password must be 8 or more characters!")
+        return;
+    }
+
+    console.log("Registration successful",{email,username,password});
+    alert("Registration Successful!");
+    registerContainer.reset();
+})
